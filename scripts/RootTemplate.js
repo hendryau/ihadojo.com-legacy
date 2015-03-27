0075bd982c7be8f1f11fc98a4cfa156940129bfa
hendryau <hendryau@gmail.com>
2015-03-26 23:21:14 -0400

    added mobile navbar

--- scripts/RootTemplate.js
+++ scripts/RootTemplate.js
@@ -9,6 +9,17 @@ angular.module('ihaDojo')
 				content: '='
 			},
 			restrict: 'E',
+			controller: 'IhaDojoCtrl',
+			controllerAs: 'rootCtrl',
 			templateUrl: 'templates/root_template.html'
 		}
+	}])
+	.controller('IhaDojoCtrl', [function() {
+		var ctrl = this;	
+		ctrl.isMobile = false;
+		
+		if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase())) {
+			ctrl.isMobile = true;
+		}
+
 	}]);
