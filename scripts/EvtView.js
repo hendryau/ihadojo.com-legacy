0822209215cc0722c164b836c272a36197d38784
hendryau <hendryau@gmail.com>
2015-03-29 23:24:41 -0400

    add upcoming evts to home page, generalize evtView directive

--- scripts/EvtView.js
+++ scripts/EvtView.js
@@ -3,20 +3,57 @@
  * 
  */
 angular.module('ihaDojo')
-	.directive('evtView', ['$http', function($http) {
+	.directive('evtView', ['$http', '$parse', function($http, $parse) {
 		return {
+			scope: {
+				showPastEvts: '&',
+				evtCount: '='
+			},
 			templateUrl: 'templates/evt_view_template.html',
-			controller: 'EvtViewCtrl',
+			controller: 'EvtCtrl',
 			controllerAs: 'ctrl', 
 			link: function(scope, element, attrs) {
+				var showPastEvts = $parse(attrs.showPastEvts)(scope);
+				var evtCount = $parse(attrs.evtCount)(scope);
+				
+				if (showPastEvts == null) {
+					scope.ctrl.showPastEvts = true;
+				}
+				else {
+					scope.ctrl.showPastEvts = showPastEvts;
+				}
+				
+				if (evtCount == null) {
+					scope.ctrl.evtCount = 9000;
+				}
+				else {
+					scope.ctrl.evtCount = evtCount;
+				}
+				
 				$http.get('data/events.json').then(function(response) {
-					scope.ctrl.evts = response.data;
+					scope.ctrl.evts = scope.ctrl.initializeEvts(response.data);
 				}); 
 			}
 		} 
 	}])
-	.controller('EvtViewCtrl', [function() {
+	.controller('EvtCtrl', [function() {
 		var ctrl = this;
+		
+		ctrl.initializeEvts = function(evts) {
+			//TODO do not assume sorted
+			var result = [];
+			for (var i=0; i<evts.length; i++) {
+				var evt = evts[i];
+				if (ctrl.isPastEvt(evt) && !ctrl.showPastEvts) {
+					continue;
+				}
+				result.push(evt);
+				if (result.length === ctrl.evtCount) {
+					break;
+				}
+			}
+			return result;
+		}
 		
 		ctrl.getDisplayDate = function(evt) {
 			var startDate = new Date(evt.start);
@@ -61,6 +98,5 @@ angular.module('ihaDojo')
 			}
 			
 			return result;
-		}
-		
+		}	
 	}]);
