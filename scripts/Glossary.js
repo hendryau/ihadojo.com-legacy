d55ce497bba2a22a51d46770670cf462ce42c9ff
hendryau <hendryau@gmail.com>
2015-04-01 18:02:56 -0400

    started glossary page

---
+++ scripts/Glossary.js
@@ -0,0 +1,20 @@
+/**
+ * @author hendryau
+ *
+ */
+angular.module('ihaDojo')
+	.directive('glossary', ['$http', function($http) {
+		return {
+			templateUrl: 'templates/directives/glossary_template.html',
+			controller: 'GlossaryCtrl',
+			controllerAs: 'ctrl',
+			link: function(scope, element, attrs) {
+				$http.get('data/glossary.json').then(function(response) {
+					scope.ctrl.glossary = response.data;
+				});
+			}
+		}
+	}])
+	.controller('GlossaryCtrl', [function() {
+		
+	}]);
