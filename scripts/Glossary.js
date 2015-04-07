f48400ae7923aa33b458415daeab271ae57b2ac6
Austin <hendryau@gmail.com>
2015-04-06 23:57:25 -0400

    glossary work

--- scripts/Glossary.js
+++ scripts/Glossary.js
@@ -5,9 +5,9 @@
 angular.module('ihaDojo')
 	.directive('glossary', ['$http', function($http) {
 		return {
-			templateUrl: 'templates/directives/glossary_template.html',
 			controller: 'GlossaryCtrl',
 			controllerAs: 'ctrl',
+			templateUrl: 'templates/directives/glossary_template.html',
 			link: function(scope, element, attrs) {
 				$http.get('data/glossary.json').then(function(response) {
 					scope.ctrl.glossary = response.data.sort(function(a, b) {
@@ -29,4 +29,46 @@ angular.module('ihaDojo')
 	}])
 	.controller('GlossaryCtrl', [function() {
 		
-	}]);
+	}])
+	.directive('term', [function() {
+		return {
+			scope: {
+				content: '='
+			},
+			templateUrl: 'templates/directives/term_template.html',
+			controller: 'TermCtrl',
+			controllerAs: 'ctrl',
+			link: function(scope, element, attrs) {
+				scope.ctrl.term = scope.content;
+			}
+		}
+	}])
+	.controller('TermCtrl', function() {
+		var ctrl = this;
+		
+		ctrl.more = false;
+		ctrl.collapsedNoteLength = 200;
+		
+		ctrl.hasNotes = function() {
+			return ctrl.term.notes != null && ctrl.term.notes.length > 0;
+		}
+		
+		ctrl.canShowMore = function() {
+			return ctrl.hasNotes() && ctrl.term.notes.length > ctrl.collapsedNoteLength;
+		}
+		
+		ctrl.displayString = function() {
+			if (ctrl.more) {
+				return ctrl.term.notes;
+			}
+			
+			return ctrl.term.notes.substring(0,ctrl.collapsedNoteLength-1).trim() + '...';
+		}
+		
+		ctrl.moreLessStr = function() {
+			if (ctrl.more) {
+				return 'less';
+			}
+			return 'more';
+		}
+	});
